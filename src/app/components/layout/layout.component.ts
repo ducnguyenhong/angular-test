5393754750219274ce74f607bc68d23fc0ce24fc
Nguyen Hong Duc <nguyenduc111299@gmail.com>
2024-10-22 15:33:11 +0700

    Update UI

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, RouterModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
